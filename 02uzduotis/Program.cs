c30046d903580135ed43354a1d5c24a9c5af5a08
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-03-29 20:30:18 +0300

    Add 02uzduotis file

---
+++ 02uzduotis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|    Mano vardas    |");
+            Console.WriteLine("|      Martynas     |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+        }
+    }
+}
