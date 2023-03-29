0f3e1e69c84689dcff14ba29978d5fa3b169a825
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-03-29 20:29:06 +0300

    Add 01uzduotis file

---
+++ 01uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "Aušra";
+            Console.WriteLine("Labas " + vardas);
+            Console.WriteLine();
+
+            int amzius = 53;
+            Console.WriteLine("Įvestas amžius: " + amzius);
+            Console.WriteLine();
+
+            int skaicius = 25;
+            Console.WriteLine($"{skaicius}{skaicius}{skaicius}{skaicius}{skaicius}");
+            Console.WriteLine();
+
+            Console.WriteLine($"{skaicius} {skaicius} {skaicius} {skaicius} {skaicius}");
+            Console.WriteLine();
+
+        }
+    }
+}
