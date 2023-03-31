2758590aeb14460301c36c8a9f8ff883fafb5788
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-03-31 12:08:45 +0300

    Add Uzduotis03 file

---
+++ Uzduotis03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uzduotis03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eile = "***********";
+            string sonai = "*         *";
+            
+            Console.WriteLine(eile);
+            Console.WriteLine(sonai);
+            Console.WriteLine(sonai);
+            Console.WriteLine(sonai);
+            Console.WriteLine(sonai);
+            Console.WriteLine(eile);
+            Console.WriteLine();
+        }
+    }
+}
