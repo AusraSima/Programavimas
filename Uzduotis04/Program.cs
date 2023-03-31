2b344c29504d686275c5f7d1345ed79745944c7a
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-03-31 12:09:45 +0300

    Add Uzduotis04 file

---
+++ Uzduotis04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string marke = "Ford";   //Automobilio marke
+            string modelis = "Ecosport";   // Automobilio modelis
+            int metai = 2019;   //Automobilio metai
+            int rida = 30590;   //Automobilio rida
+
+            Console.WriteLine();
+            Console.WriteLine($"Automobilis {marke} {modelis} ({metai} m.), rida - {rida} km.");
+            Console.WriteLine();
+        }
+    }
+}
