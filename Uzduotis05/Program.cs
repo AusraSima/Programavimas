cd7d331736083489b83babbb59ebc47219fb49fe
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-03-31 14:34:42 +0300

    Add Uzduotis05 file

---
+++ Uzduotis05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uzduotis05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Atitmetiniai veiksmai
+            int a = 8;
+            int b = 4;
+            
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{a} * {b} = {a * b}");
+            Console.WriteLine($"{a} / {b} = {a / b}");
+            Console.WriteLine();
+
+            //Kvadratas ir kubas
+            int x = 5;
+            int y = 2;
+            int z = 3;
+
+            Console.WriteLine($"{x} kvadtatu yra {Math.Pow(x, y)}");
+            Console.WriteLine($"{x} kubu yra {Math.Pow(x, z)}");
+            Console.WriteLine();
+
+            //Triju skaiciu sandauga
+            Console.WriteLine($"{x} * {y} * {z} = {x * y * z}");
+            Console.WriteLine();
+
+        }
+    }
+}
