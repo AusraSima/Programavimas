1da338a46e3592b8f1a7b34162ca910a46272794
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-04-01 21:48:20 +0300

    Add Uzduotis10 file

---
+++ Uzduotis10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uzduotis10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Iveskite skaiciu: ");
+            double skaicius = Convert.ToDouble(Console.ReadLine());
+            if (skaicius % 2 == 0)
+                Console.WriteLine($"Ivestas skaicius {skaicius}, pakeltas 5 laipsniu {Math.Pow(skaicius, 5)}");
+            if (skaicius % 4 != 0)
+                Console.Write("Iveskite antra skaiciu: ");
+                double kitas = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Abieju skaiciu suma yra {skaicius + kitas}, o sandauga {skaicius * kitas}");
+           Console.WriteLine();
+        }
+    }
+}
