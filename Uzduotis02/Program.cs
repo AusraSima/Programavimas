89d745dcff2d78f3db97a04a75a0b569984f9f63
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-04-02 19:02:50 +0300

    Add corrected Uzduotis02 file

---
+++ Uzduotis02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uzduotis02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|    Mano vardas    |");
+            Console.WriteLine("|       Ausra       |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+        }
+    }
+}
