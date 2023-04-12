6a1a9328273c871e7725261c511764e67943a077
Aušra Šimaitytė <ausrsima@yahoo.com>
2023-04-12 22:54:38 +0300

    Add file Uzduotis17

---
+++ Uzduotis17/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Uzduotis17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Teigiamo skaiciaus isvedimas mazejimo tvarka iki 0 atskirose eilutese
+            Console.Write("Iveskite teigiama skaiciu: ");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            while (skaicius > 0) 
+            {
+                Console.WriteLine(skaicius);
+                skaicius--;
+            }
+            Console.WriteLine();
+
+            //Neigiamo skaiciaus isvedimas didejimoo tvarka iki 0 vienoje eiluteje atskiriant tarpais
+            Console.Write("Iveskite neigiama skaiciu: ");
+            int neigiamas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            while (neigiamas < 0)
+            {
+                Console.Write(neigiamas + " ");
+                neigiamas++;
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Skaiciuotuvas
+            string veiksmas = "t";
+            while (veiksmas == "t") 
+            {
+                Console.WriteLine("Iveskite pirma skaiciu x: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Iveskite antra skaiciu y: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine($"{x} + {y} = {x + y}");
+                Console.WriteLine($"{x} - {y} = {x - y}");
+                Console.WriteLine($"{x} * {y} = {x * y}");
+                Console.WriteLine($"{x} / {y} = {x / y}");
+                Console.WriteLine($"{x} % {y} = {x % y}");
+                Console.WriteLine("Jei norite testi skaiciavimus, spauskite t");
+                veiksmas = Console.ReadLine();
+            }
+        }
+    }
+}
